e68a4d0d34e3e254d10f7ff672da15acec8319f8
Patrick-web <pntxall100@gmail.com>
2023-01-07 08:44:47 +0300

    Jan 7 2023

--- config.lua
+++ config.lua
@@ -244,23 +244,6 @@ lvim.plugins = {
       require("spectre").setup()
     end,
   },
-  { "zbirenbaum/copilot.lua",
-    event = { "VimEnter" },
-    config = function()
-      vim.defer_fn(function()
-        require("copilot").setup {
-          plugin_manager_path = get_runtime_dir() .. "/site/pack/packer",
-        }
-      end, 100)
-    end,
-  },
-
-  { "zbirenbaum/copilot-cmp",
-    after = { "copilot.lua", "nvim-cmp" },
-    config = function()
-      require("copilot_cmp").setup()
-    end
-  },
   {
     "simrat39/symbols-outline.nvim",
     config = function()
